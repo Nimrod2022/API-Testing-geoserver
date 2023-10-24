40dc73736acb2ce328bbf95835d7609c28328a57
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2023-10-24 19:38:21 +0200

    Downloadable CSV

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 # Define the API endpoint URL
 url = "https://api.unhcr.org/population/v1/asylum-applications/"
@@ -6,25 +7,42 @@ url = "https://api.unhcr.org/population/v1/asylum-applications/"
 # Define the parameters
 params = {
     "limit": 100000,
-    #"page": 5,
     "yearFrom": 2013,
     "yearTo": 2022,
     "coo": "UKR",
     "coa": "AUT",
-    # "coo_all": False,
-    # "coa_all": False,
     "cf_type": "iso"
 }
 
-# Make a GET request to the API
+# GET request to the API
 response = requests.get(url, params=params)
 
-# Check if the request was successful (status code 200)
+# Check if the request was successful
 if response.status_code == 200:
     # Parse the JSON response
     data = response.json()
 
-    # Do something with the data, for example, print it
-    print(data)
+    # Define the output CSV file name
+    output_csv_filename = "asylum_data_output.csv"
+
+    # Open the CSV file for writing
+    with open(output_csv_filename, 'w', newline='') as csvfile:
+        # Create a CSV writer
+        csv_writer = csv.writer(csvfile)
+
+        # Write the header row
+        csv_writer.writerow(["year", "coo_name", "coa_name", "applied"])
+
+        # Extract and write data to the CSV file
+        for item in data.get("items", []):
+            year = item.get("year", "")
+            coo_name = item.get("coo_name", "")
+            coa_name = item.get("coa_name", "")
+            applied = item.get("applied", "")
+
+            # Write the data as a row in the CSV file
+            csv_writer.writerow([year, coo_name, coa_name, applied])
+
+    print(f"Data has been written to {output_csv_filename}.")
 else:
     print("Failed to retrieve data. Status code:", response.status_code)

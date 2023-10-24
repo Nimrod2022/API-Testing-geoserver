ffa2401857d65e340df29f2239e998d615d27614
Nimrod Kibet <nimrodmandelakibet@gmail.com>
2023-10-24 20:41:25 +0200

    Multiple countries implemented

--- main.py
+++ main.py
@@ -5,44 +5,62 @@ import csv
 url = "https://api.unhcr.org/population/v1/asylum-applications/"
 
 # Define the parameters
-params = {
-    "limit": 100000,
-    "yearFrom": 2013,
-    "yearTo": 2022,
-    "coo": "UKR",
-    "coa": "AUT",
-    "cf_type": "iso"
-}
+year_range = range(2013, 2023)
+coo = "UKR"
+coa_countries = ["AUT", "DEU", "ITA", "GBR", "SWE", "SWI"]
 
-# GET request to the API
-response = requests.get(url, params=params)
+# Define the output CSV file name
+output_csv_filename = "asylum_data_output.csv"
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
+# Open the CSV file for writing
+with open(output_csv_filename, 'w', newline='') as csvfile:
+    # Create a CSV writer
+    csv_writer = csv.writer(csvfile)
 
-    # Define the output CSV file name
-    output_csv_filename = "asylum_data_output.csv"
+    # Write the header row
+    csv_writer.writerow(["year", "coo_name", "coa_name", "applied"])
 
-    # Open the CSV file for writing
-    with open(output_csv_filename, 'w', newline='') as csvfile:
-        # Create a CSV writer
-        csv_writer = csv.writer(csvfile)
+    # Loop through the coa_countries
+    for coa in coa_countries:
+        for year in year_range:
+            page = 1
+            while True:
+                # Define the parameters for the API request, including the page
+                params = {
+                    "limit": 100,  # Adjust the limit per page as needed
+                    "yearFrom": year,
+                    "yearTo": year,
+                    "coo": coo,
+                    "coa": coa,
+                    "cf_type": "iso",
+                    "page": page
+                }
 
-        # Write the header row
-        csv_writer.writerow(["year", "coo_name", "coa_name", "applied"])
+                # Make a GET request to the API
+                response = requests.get(url, params=params)
 
-        # Extract and write data to the CSV file
-        for item in data.get("items", []):
-            year = item.get("year", "")
-            coo_name = item.get("coo_name", "")
-            coa_name = item.get("coa_name", "")
-            applied = item.get("applied", "")
+                # Check if the request was successful (status code 200)
+                if response.status_code == 200:
+                    # Parse the JSON response
+                    data = response.json()
 
-            # Write the data as a row in the CSV file
-            csv_writer.writerow([year, coo_name, coa_name, applied])
+                    # Extract and write data to the CSV file
+                    for item in data.get("items", []):
+                        coo_name = item.get("coo_name", "")
+                        coa_name = item.get("coa_name", "")
+                        applied = item.get("applied", "")
 
-    print(f"Data has been written to {output_csv_filename}.")
-else:
-    print("Failed to retrieve data. Status code:", response.status_code)
+                        # Write the data as a row in the CSV file
+                        csv_writer.writerow([year, coo_name, coa_name, applied])
+
+                    # Check if there are more pages
+                    if data.get("maxPages", 1) <= page:
+                        break
+                    else:
+                        page += 1
+                else:
+                    print(f"Failed to retrieve data for coa={coa}, year={year}, page={page}. Status code:",
+                          response.status_code)
+                    break
+
+print(f"Data has been written to {output_csv_filename}.")
